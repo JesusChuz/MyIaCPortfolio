9db64016185f0a938f4075e1807d238efab0a807
JesusChuz <j.mendezquesada18@gmail.com>
2025-09-30 15:37:14 -0600

    add container app

---
+++ containerapp-acr/main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# -------------------------------
+# Resource Group
+# -------------------------------
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-containerapp-demo"
+  location = "East US"
+}
+
+# -------------------------------
+# Container App Environment
+# -------------------------------
+resource "azurerm_container_app_environment" "env" {
+  name                = "cae-demo-env"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# -------------------------------
+# Container App
+# -------------------------------
+resource "azurerm_container_app" "app" {
+  name                         = "demoapp"
+  resource_group_name          = azurerm_resource_group.rg.name
+  container_app_environment_id = azurerm_container_app_environment.env.id
+  revision_mode                = "Single"
+
+  template {
+    container {
+      name   = "demo-container"
+      image  = "mcr.microsoft.com/azuredocs/containerapps-helloworld:latest"
+      cpu    = 0.25
+      memory = "0.5Gi"
+    }
+  }
+
+  ingress {
+    external_enabled = true # exposes a public endpoint
+    target_port      = 80
+    transport        = "auto"
+    traffic_weight {
+      latest_revision = true
+      percentage      = 100
+    }
+  }
+}
