3b095c8c3146cdea9bd4cd3a329f9ee73ac53df4
JesusChuz <j.mendezquesada18@gmail.com>
2025-10-02 16:53:11 -0600

    add output segments in main

--- pythonappserviceJenkins/main.tf
+++ pythonappserviceJenkins/main.tf
@@ -70,3 +70,16 @@ resource "azurerm_linux_web_app_slot" "staging" {
     }
   }
 }
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "app_service_name" {
+  value = azurerm_linux_web_app.app.name
+}
+
+output "slot_name" {
+  value = azurerm_linux_web_app_slot.staging.name
+}
+
