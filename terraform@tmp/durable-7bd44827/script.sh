256ba8bdc02d840eca2d3bd86b4c62bdd5b04509
JesusChuz <j.mendezquesada18@gmail.com>
2025-10-03 08:57:45 -0600

    remove terraform@tmp/durable-7bd44827

--- terraform@tmp/durable-7bd44827/script.sh
+++
@@ -1,8 +0,0 @@
-
-                            az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET -t $AZURE_TENANT_ID
-                            az account set -s $AZURE_SUBSCRIPTION_ID
-                            az resource list
-                            terraform init
-                            terraform plan -out=tfplan
-                            terraform apply -auto-approve tfplan
-                        
